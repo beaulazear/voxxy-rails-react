bd52ad00811f7b37c9ecb7928f0b72a860b93b03
Beau Lazear <beaulazear@gmail.com>
2024-11-05 23:32:40 -0500

    added bin build commands

---
+++ bin/render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# builds the front end code
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+# builds the back end code
+bundle install
+bundle exec rake db:migrate
