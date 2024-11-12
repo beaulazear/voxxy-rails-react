212f5d1a36653fd0d8a7db4531324d4f7ade2fda
Beau Lazear <beaulazear@gmail.com>
2024-11-11 19:17:08 -0500

    changing application.rb again to work on production cookies

--- config/application.rb
+++ config/application.rb
@@ -8,24 +8,21 @@ module VoxxyRails
   class Application < Rails::Application
     # API-only mode
     config.api_only = true
-    
-    # Adding cookies and session middleware
+
+    # Adding cookies and session middleware for API-only app
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
 
-    # Configure the session store with SameSite=None and Secure attributes in production
-    # config.session_store :cookie_store, key: '_voxxy_session', same_site: :none, secure: Rails.env.production?
-
-    # Ensure SameSite attribute is None for cookies during development and production
+    # Use SameSite=Strict for all cookies, similar to previous configuration
     config.action_dispatch.cookies_same_site_protection = :strict
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
-    # CORS configuration for both development and production
+    # CORS configuration for development and production
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000', 'https://www.voxxyai.com' # Development and Production URLs
+        origins 'http://localhost:3000', 'https://www.voxxyai.com' # Update with your frontend URLs
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
