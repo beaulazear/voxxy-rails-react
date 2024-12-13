e78cc2d260ba15e3e243dc3583fc05245f04ef10
Beau Lazear <beaulazear@gmail.com>
2024-12-13 16:56:39 -0500

    removed destroy users lines in build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,8 +10,4 @@ cp -a client/build/. public/
 # builds the back end code
 bundle install
 
-#remove below code immediatly after next push
-echo "Destroying all users..."
-bundle exec rails runner "User.destroy_all"
-
 bundle exec rake db:migrate
