94f2fc1672ca15e63339eeb31ececa69e518b751
Beau Lazear <beaulazear@gmail.com>
2025-02-11 22:22:05 -0500

    gmail.com oops i meant to say i wiped production database for a fresh start and this is me removing the code to do that.'

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -12,17 +12,3 @@ bundle install
 
 # Runs database migrations
 bundle exec rake db:migrate
-
-# If in production, create a default user
-if [ "$RAILS_ENV" = "production" ]; then
-  echo "Creating default user..."
-  bundle exec rails runner "User.create!(
-    name: 'Testing',
-    username: 'testinguser',
-    email: 'testing@gmail.com',
-    password: 'testingpass',
-    password_confirmation: 'testingpass',
-    confirmed_at: Time.current
-  ) unless User.exists?(email: 'testing@gmail.com')"
-  echo "Default user created!"
-fi
