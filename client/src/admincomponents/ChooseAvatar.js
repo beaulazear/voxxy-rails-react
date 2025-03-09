311c95d8e7c95e57c7d20db574f0558735d5a834
Beau Lazear <beaulazear@gmail.com>
2025-03-09 11:37:15 -0400

    add new avatars and attribute for vacteezy udnerneath

--- client/src/admincomponents/ChooseAvatar.js
+++ client/src/admincomponents/ChooseAvatar.js
@@ -7,6 +7,11 @@ import Avatar3 from "../assets/Avatar3.jpg";
 import Avatar4 from "../assets/Avatar4.jpg";
 import Avatar5 from "../assets/Avatar5.jpg";
 import Avatar6 from "../assets/Avatar6.jpg";
+import Avatar7 from "../assets/Avatar7.jpg";
+import Avatar8 from "../assets/Avatar8.jpg";
+import Avatar9 from "../assets/Avatar9.jpg";
+import Avatar10 from "../assets/Avatar10.jpg";
+import Avatar11 from "../assets/Avatar11.jpg";
 
 const avatars = [
   { id: 1, src: Avatar1, path: "/assets/Avatar1.jpg" },
@@ -15,6 +20,11 @@ const avatars = [
   { id: 4, src: Avatar4, path: "/assets/Avatar4.jpg" },
   { id: 5, src: Avatar5, path: "/assets/Avatar5.jpg" },
   { id: 6, src: Avatar6, path: "/assets/Avatar6.jpg" },
+  { id: 7, src: Avatar7, path: "/assets/Avatar7.jpg" },
+  { id: 8, src: Avatar8, path: "/assets/Avatar8.jpg" },
+  { id: 9, src: Avatar9, path: "/assets/Avatar9.jpg" },
+  { id: 10, src: Avatar10, path: "/assets/Avatar10.jpg" },
+  { id: 11, src: Avatar11, path: "/assets/Avatar11.jpg" },
 ];
 
 const AvatarContainer = styled.div`
@@ -159,6 +169,7 @@ export default function ChooseAvatar() {
           </AvatarButton>
         ))}
       </AvatarGrid>
+      <a target="_blank" rel="noreferrer" href="https://www.vecteezy.com/free-vector/avatar">Avatar Vectors by Vecteezy</a>
     </AvatarContainer>
   );
 }
