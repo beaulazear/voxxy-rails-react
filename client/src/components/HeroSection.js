c6a930fefb2e030454277c2ce6fac21ac1a31ba9
Beau Lazear <beaulazear@gmail.com>
2025-03-12 17:11:00 -0400

    more styling on landing page

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -17,7 +17,7 @@ const HeroContainer = styled.section`
     position: absolute;
     width: 60vw; /* Wider to create an oval shape */
     height: 35vh; /* Shorter height for an elliptical effect */
-    background: radial-gradient(ellipse at center, rgba(138, 43, 226, 0.3) 10%, rgba(255, 255, 255, 0) 60%);
+    background: radial-gradient(ellipse at center, rgba(138, 43, 226, 0.25) 10%, rgba(255, 255, 255, 0) 60%);
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
@@ -26,17 +26,12 @@ const HeroContainer = styled.section`
   }
 
   @media (max-width: 768px) {
-    padding: 5rem 2rem;
-
-    &::before {
-      width: 80vw; /* Adjust for mobile screens */
-      height: 40vh;
-    }
+    padding: 4rem 1.5rem;
   }
 `;
 
 const HeroTitle = styled.h1`
-  font-size: clamp(1.5rem, 5vw, 3rem);
+  font-size: clamp(1.7rem, 5vw, 3.2rem);
   font-weight: bold;
   margin-bottom: 0.4rem;
   color: #000;
